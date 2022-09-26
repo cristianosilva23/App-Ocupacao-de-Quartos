5ca298fb2527f689a961c12d45d728f95fae4914
Cristiano Silva <84424693+cristianosilva23@users.noreply.github.com>
2022-09-26 13:24:19 -0300

    Add files via upload

---
+++ AppQuartos/AppQuartos/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace AppQuartos {
+    internal class Pessoa {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Pessoa(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Nome + ", "+ Email;
+        }
+
+    }
+}
---
+++ AppQuartos/AppQuartos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+namespace AppQuartos {
+    class Program {
+        static void Main(string[] args) {
+
+            Pessoa[] quartos = new Pessoa[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int qtdQuartos = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= qtdQuartos; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i};");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                quartos[room] = new Pessoa(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++) {
+                if (quartos[i] != null) {
+                    Console.WriteLine(i + ": " + quartos[i]);
+                }
+            }
+        }
+    }
+}
